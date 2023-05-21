7f0a7a4294677ff5510c22f81b4472a49edfc270
piedor <dorighi.pietro@hotmail.it>
2023-05-21 20:57:06 +0200

    Commenti

--- static/script.js
+++ static/script.js
@@ -1,103 +1,107 @@
-/**
- * This variable stores the logged in user
- */
+// Memorizza l'utente loggato
 var loggedUser = {};
+// Memorizza l'utente registrato
 var registeredUser = {};
 
-/**
- * This function is called when login button is pressed.
- * Note that this does not perform an actual authentication of the user.
- * A student is loaded given the specified email,
- * if it exists, the studentId is used in future calls.
- */
 function login()
 {
-    //get the form object
-    var nickname = document.getElementById("loginNickname").value;
-    var password = document.getElementById("loginPassword").value;
-    // console.log(email);
+  // Questa funzione è chiamata quando si preme il pulsante login
+  // Prendi i valori del form html
+  var nickname = document.getElementById("loginNickname").value;
+  var password = document.getElementById("loginPassword").value;
 
-    fetch('../api/v1/authentications', {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify( { nickname: nickname, password: password } ),
-    })
-    .then((resp) => resp.json()) // Transform the data into json
-    .then(function(data) { // Here you get the data to modify as you please
-        //console.log(data);
-        loggedUser.token = data.token;
-        loggedUser.email = data.email;
-        loggedUser.id = data.id;
-        loggedUser.self = data.self;
-        // loggedUser.id = loggedUser.self.substring(loggedUser.self.lastIndexOf('/') + 1);
-        //document.getElementById("loggedUser").textContent = loggedUser.email;
-        alert(data.message);
-        if (data.success){
-          location.href = "home_aut.html";
-        }
-        return;
-    })
-    .catch( function (error) {
-        alert(error.message);
-        return;
-      } ); // If there is any error you will catch them here
+  // Richiama l'API authentication
+  fetch('../api/v1/authentications', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify( { nickname: nickname, password: password } ),
+  })
+  .then((resp) => resp.json()) // Trasforma i dati in json
+  .then(function(data) { // Ricevi la risposta
+      loggedUser.token = data.token;
+      loggedUser.email = data.email;
+      loggedUser.id = data.id;
+      loggedUser.self = data.self;
+      // Popup con messaggio API
+      alert(data.message);
+      if (data.success){
+        // Autenticato correttamente passa a home autenticato
+        location.href = "home_aut.html";
+      }
+      return;
+  })
+  .catch( function (error) {
+      alert(error.message);
+      return;
+    } );
 
 };
 
 function containsNumbers(str) {
-    return /[0-9]/.test(str);
-  };
+  // Ritorna se str contiene numeri
+  return /[0-9]/.test(str);
+};
+
 function containsUppercase(str) {
-    return /[A-Z]/.test(str);
-  };
+  // Ritorna se str contiene lettere maiuscole
+  return /[A-Z]/.test(str);
+};
+
 function containsLowercase(str) {
-    return /[a-z]/.test(str);
-  };
+  // Ritorna se str contiene lettere minuscole
+  return /[a-z]/.test(str);
+};
+
 function isValidEmail(mail) 
 {
-  var mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
-  return(mail.value.match(mailformat));
+  // Ritorna se mail è una mail valida
+  var mailFormat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+  return(mail.value.match(mailFormat));
 }
 
 function manageMail(mail) {
-    var btMail = document.getElementById('cmail');
-    // Controllo campo mail che non sia vuoto e valido
-    if (isValidEmail(mail)) {
-        btMail.disabled = false;
-        document.getElementById("regEmail").setAttribute("style","background: rgb(76, 249, 73);");
-    }
-    else {
-        btMail.disabled = true;
-        document.getElementById("regEmail").setAttribute("style","background: rgb(253, 116, 116);");
-        //rendere bordo input mail rosso
-    }
+  // Chiamata quando il campo mail in registrazione cambia
+  // Se mail valida allora bordo verde altrimenti bordo rosso
+  var btMail = document.getElementById('cmail');
+  // Controllo campo mail che non sia vuoto e valido
+  if (isValidEmail(mail)) {
+      btMail.disabled = false;
+      // Rendere bordo input mail verde
+      document.getElementById("regEmail").setAttribute("style","background: rgb(76, 249, 73);");
   }
-//function to generate and send the code to confirm the mail address
-{
-var vcode;
-function generaCodiceConfermaMail()
-{
-  vcode=Math.floor(Math.random() * (99999999 - 10000000 + 1)) + 10000000;
-  var mail=document.getElementById("regEmail").value;
-  sendMails(mail,"codicec", vcode);
-  alert("Ti abbiamo inviato un codice di conferma nell'indirizzo mail indicato, inserisci quel codice qui sotto e premi ok per confermare la mail. ATTENZIONE: una volta confermata la mail non si potrà più modificare.");
-  document.getElementById("regEmail").disabled = true;
-  document.getElementById("confcode").removeAttribute("hidden");
-  document.getElementById("confebtn").removeAttribute("hidden");
-  document.getElementById("confebtn").removeAttribute("disabled");
-};
+  else {
+      btMail.disabled = true;
+      // Rendere bordo input mail rosso
+      document.getElementById("regEmail").setAttribute("style","background: rgb(253, 116, 116);");
+  }
+}
 
-function confermaCodiceConfermaMail()
+// Funzione per generare e inviare il codice per confermare la mail
 {
-  var code=document.getElementById("confcode").value;
-  if(code==vcode){
-    alert("mail confermata"); 
-    document.getElementById("confermaR").disabled = false;
-    document.getElementById("cmail").disabled = true;
-    document.getElementById("confcode").setAttribute("disabled","true");
-    document.getElementById("confebtn").setAttribute("disabled","true");
-  }
-};
+  var vcode;
+  function generaCodiceConfermaMail()
+  {
+    vcode = Math.floor(Math.random() * (99999999 - 10000000 + 1)) + 10000000;
+    var mail=document.getElementById("regEmail").value;
+    sendMails(mail,"codicec", vcode);
+    alert("Ti abbiamo inviato un codice di conferma nell'indirizzo mail indicato, inserisci quel codice qui sotto e premi ok per confermare la mail. ATTENZIONE: una volta confermata la mail non si potrà più modificare.");
+    document.getElementById("regEmail").disabled = true;
+    document.getElementById("confcode").removeAttribute("hidden");
+    document.getElementById("confebtn").removeAttribute("hidden");
+    document.getElementById("confebtn").removeAttribute("disabled");
+  };
+
+  function confermaCodiceConfermaMail()
+  {
+    var code = document.getElementById("confcode").value;
+    if(code == vcode){
+      alert("mail confermata"); 
+      document.getElementById("confermaR").disabled = false;
+      document.getElementById("cmail").disabled = true;
+      document.getElementById("confcode").setAttribute("disabled","true");
+      document.getElementById("confebtn").setAttribute("disabled","true");
+    }
+  };
 }
 
 function clearAll(){
@@ -107,88 +111,91 @@ function clearAll(){
   document.getElementById("cmail").removeAttribute("style");
   document.getElementById("confcode").disabled=false;
 }
+
 function register()
 {
-    //get the form object
-    var nickname = document.getElementById("regNickname").value;
-    var email = document.getElementById("regEmail").value;
-    var password = document.getElementById("regPassword").value;
-    var cpassword = document.getElementById("regC_Password").value;
-    var avatar = document.querySelector('input[type = radio]:checked').value;
-    var zona = document.getElementById("zona").value;
-    var bio = document.getElementById("bio").value;
-    var privato = document.getElementById("switch").value; 
-    var preferenze=[];
-    var markedCheckbox = document.getElementsByName('pref');  
-    for (var checkbox of markedCheckbox) {  
-      if (checkbox.checked) 
-      preferenze.push(checkbox.value);
-    }  
-    var piattaforme=[];
-    var markedCheckbox = document.getElementsByName('piatt');  
-    for (var checkbox of markedCheckbox) {  
-      if (checkbox.checked) 
-      preferenze.push(checkbox.value);
-    }  
-    
-    document.getElementById("errors").innerHTML = "";
-    var errors="";
-    // console.log(email);
-    if(nickname=="") errors+="nickname mancante; ";
-    if(email=="") errors+="email mancante; ";
-    if(password == "") errors+="la password è mancante; ";
-        else{
-        if(password!=cpassword) errors+="password e conferma password devono essere uguali; ";
-        if(password.length<8) errors+="la password deve essere di almeno 8 caratteri; ";
-        if(!containsUppercase(password)) errors+="la password deve contenere almeno un carattere maiuscolo; ";
-        if(!containsLowercase(password)) errors+="la password deve contenere almeno un carattere minuscolo; ";
-        if(!containsNumbers(password)) errors+="la password deve contenere almeno un numero; ";
-    }
-    if(preferenze.length == 0) errors+="devi selezionare almeno una preferenza; ";
-    
+  // Questa funzione è chiamata durante la fase di registrazione
+  var nickname = document.getElementById("regNickname").value;
+  var email = document.getElementById("regEmail").value;
+  var password = document.getElementById("regPassword").value;
+  var cpassword = document.getElementById("regC_Password").value;
+  var avatar = document.querySelector('input[type = radio]:checked').value;
+  var zona = document.getElementById("zona").value;
+  var bio = document.getElementById("bio").value;
+  var privato = document.getElementById("switch").value; 
+  var preferenze = [];
+  var markedCheckbox = document.getElementsByName('pref');  
+  for (var checkbox of markedCheckbox) {  
+    if (checkbox.checked) 
+    preferenze.push(checkbox.value);
+  }  
+  var piattaforme = [];
+  var markedCheckbox = document.getElementsByName('piatt');  
+  for (var checkbox of markedCheckbox) {  
+    if (checkbox.checked) 
+    preferenze.push(checkbox.value);
+  }  
+  
+  document.getElementById("errors").innerHTML = "";
+  var errors = "";
+  if(nickname == "") errors += "nickname mancante; ";
+  if(email == "") errors += "email mancante; ";
+  if(password == "") errors += "la password è mancante; ";
+      else{
+      if(password != cpassword) errors += "password e conferma password devono essere uguali; ";
+      if(password.length < 8) errors += "la password deve essere di almeno 8 caratteri; ";
+      if(!containsUppercase(password)) errors += "la password deve contenere almeno un carattere maiuscolo; ";
+      if(!containsLowercase(password)) errors += "la password deve contenere almeno un carattere minuscolo; ";
+      if(!containsNumbers(password)) errors += "la password deve contenere almeno un numero; ";
+  }
+  if(preferenze.length == 0) errors += "devi selezionare almeno una preferenza; ";
+  
 
-    if(errors!=""){
-        errors="errori presenti: "+errors;
-        errors=String(errors);
-        document.getElementById("errors").innerHTML = errors;
-        return;
-    }
-    fetch('../api/v1/registrations', {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify( { 
-          nickname: nickname, 
-          email: email, 
-          password: password,
-          bio: bio,
-          preferenze: preferenze,
-          piattaforme: piattaforme
-        } ),
-    })
-    .then((resp) => resp.json()) // Transform the data into json
-    .then(function(data) { // Here you get the data to modify as you please
-        //console.log(data);
-        registeredUser.email = data.email;
-        registeredUser.id = data.id;
-        registeredUser.self = data.self;
-        // loggedUser.id = loggedUser.self.substring(loggedUser.self.lastIndexOf('/') + 1);
-        alert(data.message);
-        if(data.success){
-          sendMails(email,"registrazionec", nickname);
-        }
-        return;
-    })
-    .catch( function (error) {
-            alert(error.message);
-            console.error(error);
-            return;
-        } ); // If there is any error you will catch them here
+  if(errors != ""){
+      errors = "errori presenti: " + errors;
+      errors = String(errors);
+      document.getElementById("errors").innerHTML = errors;
+      return;
+  }
+
+  // Richiama l'API registration
+  fetch('../api/v1/registrations', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify( { 
+        nickname: nickname, 
+        email: email, 
+        password: password,
+        bio: bio,
+        preferenze: preferenze,
+        piattaforme: piattaforme
+      } ),
+  })
+  .then((resp) => resp.json()) // Trasforma i dati in JSON
+  .then(function(data) { // Risposta
+      registeredUser.email = data.email;
+      registeredUser.id = data.id;
+      registeredUser.self = data.self;
+      // Popup messaggio API
+      alert(data.message);
+      if(data.success){
+        // Registrazione ok
+        // Invia mail 
+        sendMails(email,"registrazionec", nickname);
+      }
+      return;
+  })
+  .catch( function (error) {
+          alert(error.message);
+          console.error(error);
+          return;
+      } );
         
 };
 
-
 function sendMails(toMail, oggetto, txt)
 {
+  // Questa funzione richiama l'API sendMails per inviare una mail
   fetch('../api/v1/sendMails', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
@@ -199,16 +206,18 @@ function sendMails(toMail, oggetto, txt)
 function loadInfoUser(){
   // Viene usata in home_aut per mostrare le info dell'utente
   fetch('/api/v1/utenti/me')
-  .then((resp) => resp.json()) // Transform the data into json
-  .then(function(data) { // Here you get the data to modify as you please
+  .then((resp) => resp.json()) // Trasforma i dati in JSON
+  .then(function(data) { // Risposta
       if(data.success == false){
-          alert("Errore non sei autenticato!");
-          location.href = "/";
+        // Non è autenticato ritorna su home non autenticato
+        alert("Errore non sei autenticato!");
+        location.href = "/";
       }
       else{
-          var nickname = data.nickname;
-          document.getElementById("nickname").textContent = nickname;
-          document.body.removeAttribute("hidden");
+        // Autenticato mostra info
+        var nickname = data.nickname;
+        document.getElementById("nickname").textContent = nickname;
+        document.body.removeAttribute("hidden");
       }
   })
   .catch( error => console.error(error) );
@@ -217,8 +226,9 @@ function loadInfoUser(){
 function logout(){
   // Funzione per eseguire il logout dell'utente (in pratica rimuove il token dai cookie)
   fetch('/api/v1/utenti/logout')
-  .then((resp) => resp.json()) // Transform the data into json
-  .then(function(data) { // Here you get the data to modify as you please
+  .then((resp) => resp.json()) // Trasforma i dati in JSON
+  .then(function(data) { // Risposta
+    // Ritorna su home non autenticato
     alert(data.message);
     location.href = "/";
   })
