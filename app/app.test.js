f9fbe90cb808dc2b85f31fc646eea96f4eca8944
piedor <dorighi.pietro@hotmail.it>
2023-05-23 09:15:57 +0200

    Implementato Jest con variabili ambiente dotenv, primo test app

---
+++ app/app.test.js
@@ -0,0 +1,13 @@
+const request = require('supertest');
+const jwt     = require('jsonwebtoken');
+const app     = require('./app');
+
+test('Modulo app definito', () => {
+  expect(app).toBeDefined();
+});
+
+test('GET / ritorna 200', () => {
+  return request(app)
+    .get('/')
+    .expect(200);
+});
