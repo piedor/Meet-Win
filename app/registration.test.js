61a148e0d485b4e20ea2aa90855bc4cd00a8afb6
nick <nicholas.menin>
2023-05-31 17:39:59 +0200

    Registration_Testing_1

---
+++ app/registration.test.js
@@ -0,0 +1,17 @@
+// FUNZIONI DI INTRO... cosa va messo?
+
+/*
+describe('COSA METTERE QUI?', () => {
+
+    BEFORE ALL... cosa sta succedendo qui? apre connessione con il db e poi la chiude?
+    AFTER ALL...
+
+    test(' ... registration con campo nickname vuoto', () => {
+        return request(app)
+          .post('/api/v1/registrations')
+          .set('Accept', 'application/json')    ?????
+          .expect(200, { success: false, message: "Controlla di aver inserito tutti i dati richiesti!" });
+    })
+
+});
+*/
